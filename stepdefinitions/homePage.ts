08816754b76383c2d6037000716a6ede3b326b9a
ericweiler <ericw3@vt.edu>
2017-08-01 14:29:33 -0400

    Update homePage.ts

--- stepdefinitions/homePage.ts
+++ stepdefinitions/homePage.ts
@@ -10,4 +10,16 @@ defineSupportCode(function ({ Given }) {
     Given(/^I am on google page$/, async () => {
         await expect(browser.getTitle()).to.eventually.equal('Google');
     });
+    
+    When(/^I type "(.*?)"$/, async (text) => {
+        await search.searchTextBox.sendKeys(text);
+    });
+
+    Then(/^I click on search button$/, async () => {
+        await search.searchButton.click();
+    });
+    
+    Then(/^I clear the search text$/, async () => {
+        await search.searchTextBox.clear();
+    });
 })
